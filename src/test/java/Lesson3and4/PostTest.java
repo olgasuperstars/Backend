5678078af45eed46cc8dd22ffbef86f3b4ea57ec
olgasuperstars <olgasuperstar@mail.ru>
2022-06-21 11:16:37 +0300

    Lesson 3 and lesson 4

--- src/test/java/Lesson3and4/PostTest.java
+++ src/test/java/Lesson3and4/PostTest.java
@@ -34,72 +34,34 @@ public class PostTest {
                 .jsonPath()
                 .get("id")
                 .toString();
-
-
-
-    }
-
-    @AfterEach
-    void dropDown() {
         given()
                 .queryParam("apiKey", "d574174118a34a01a89f71701083d4ff")
                 .queryParam("hash", "a3da66460bfb7e62ea1c96cfa0b7a634a346ccbf")
                 .delete("https://api.spoonacular.com/mealplanner/geekbrains/items/" + id)
                 .then()
                 .statusCode(200);
-    }
-    @Test
-    @DisplayName("ClassifyCuisine")
-    void classifyCuisine() {
-        id = given()
-                .queryParam("apiKey", "d574174118a34a01a89f71701083d4ff")
-                .body("{\n"
-                        + "    \"cuisine\": Mediterranean,\n"
-                        + "        \"cuisines\": [\n"
-                        + "      {\n"
-                        + "             \"Mediterranean,\""
-                        + "              \"European,\""
-                        + "               \"Italian,\""
-                        + "            }\n"
-                        + "         ]\n"
-                        + "     \"confidence\": 0.0 \n"
-                        + "    }\n"
-                        + "}")
-                .when()
-                .post("https://api.spoonacular.com/recipes/cuisine/")
-                .then()
-                .statusCode(200)
-                .extract()
-                .jsonPath()
-                .get("id")
-                .toString();
+
 
     }
+
+
+
 
     @Test
     @DisplayName("Compute Glycemic Load")
     void computeGlycemicLoad() {
-        id = given()
+        String value = given()
                 .queryParam("apiKey", "d574174118a34a01a89f71701083d4ff")
-                .body("{\n"
-                        + "        \"ingredients\": [\n"
-                        + "           {\n"
-                        + "             \"1 kiwi,\""
-                        + "              \"2 cups rice,\""
-                        + "               \"2 glasses of water\""
-                        + "            }\n"
-                        + "         ]\n"
-                        + "    }\n"
-                        + "}")
+                .body("{ \"ingredients\":[ \"1 kiwi\", \"2 cups rice\", \"2 glasses of water\" ] }")
                 .when()
-                .post("https://api.spoonacular.com/food/ingredients/glycemicLoad/")
+                .post("https://api.spoonacular.com/food/ingredients/glycemicLoad")
+                .prettyPeek()
                 .then()
                 .statusCode(200)
                 .extract()
                 .jsonPath()
-                .get("id")
+                .get("totalGlycemicLoad")
                 .toString();
-
 
 
     }
